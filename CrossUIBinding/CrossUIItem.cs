6a2cdb259adff7a08a3ad9c11754f265ecd97dcd
rudy <rudy@dropbox.com>
2018-03-16 01:20:15 -0700

    add missing files

---
+++ CrossUIBinding/CrossUIItem.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+
+namespace Huyn.CrossUIBinding
+{
+    public class CrossUIItem<T>
+    {
+        public event EventHandler Updated;
+        private T _item;
+        private bool _autoRaisePropertyChanged;
+        public CrossUIItem(T property = default(T), bool autoRaisePropertyChanged = false)
+        {
+            _item = property;
+            _autoRaisePropertyChanged = autoRaisePropertyChanged;
+        }
+
+        public T Value
+        {
+            get
+            {
+                return _item;
+            }
+            set
+            {
+                SetValue(value);
+            }
+        }
+
+        public Dictionary<CoreDispatcher, CrossUIItemClone<T>> _clones = new Dictionary<CoreDispatcher, CrossUIItemClone<T>>();
+
+        public CrossUIItemClone<T> Clone
+        {
+            get
+            {
+                CrossUIItemClone<T> val;
+                var dispatcher = Window.Current.Dispatcher;
+                if (_clones.TryGetValue(dispatcher, out val))
+                {
+                    return val;
+                }
+                else
+                {
+                    val = new CrossUIItemClone<T>(this, dispatcher);
+                    _clones[dispatcher] = val;
+
+                    ApplicationView.GetForCurrentView().Consolidated += (sender, e) =>
+                      {
+                          if (e.IsAppInitiated || e.IsUserInitiated)
+                          {
+                              CrossUIItemClone<T> cloneToUnlink;
+                              if (_clones.TryGetValue(dispatcher, out cloneToUnlink))
+                              {
+                                  _clones.Remove(dispatcher);
+                                  cloneToUnlink.Unlink();
+                              }
+                          }
+                      };
+                    return val;
+                }
+            }
+        }
+
+
+        public void SetValue(T value)
+        {
+            if (_item == null)
+            {
+                if (value == null)
+                    return;
+            }
+            else if (_item.Equals(value))
+                return;
+
+            _item = value;
+            if(_autoRaisePropertyChanged)
+                Updated?.Invoke(this, null);
+        }
+
+        public void RaisePropertyChanged()
+        {
+            Updated?.Invoke(this, null);
+        }
+    }
+}
---
+++ CrossUIBinding/CrossUIItemClone.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel;
+using Windows.UI.Core;
+
+namespace Huyn.CrossUIBinding
+{
+    
+    public class CrossUIItemClone<T> : INotifyPropertyChanged
+    {
+
+        private readonly bool _autoRaisePropertyChanged;
+        private readonly CoreDispatcher _dispatcher;
+        private readonly CrossUIItem<T> _source;
+
+        internal CrossUIItemClone(CrossUIItem<T> source, CoreDispatcher dispatcher)
+        {
+            _source = source;
+            _dispatcher = dispatcher;
+            _source.Updated += Source_Updated;
+
+        }
+        public T Value
+        {
+            get
+            {
+                return _source.Value;
+            }
+            set
+            {
+                _source.SetValue(value);
+            }
+        }
+
+        internal void Unlink()
+        {
+            this._source.Updated -= Source_Updated;
+        }
+
+        private void Source_Updated(object sender, object s)
+        {
+            try
+            {
+                _dispatcher?.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Value)));
+                });
+            }
+            catch(Exception)
+            {
+                _source.Updated -= Source_Updated;
+
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
